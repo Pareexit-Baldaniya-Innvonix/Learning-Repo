6a1f9211acb11e548c7abc5863b7945f40cd60ea
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-01-08 14:51:47 +0530

    practice OOPs program

---
+++ test2.py
@@ -0,0 +1,22 @@
+# OOPs program
+
+class Pet:
+    def __init__(self, name):
+        self.name = name
+        self.hunger_level = 50
+
+    def eat(self):
+        self.hunger_level -= 10
+
+    def check_status(self):
+        if self.hunger_level <= 20:
+            print(f"{self.name} is full and happy!")
+        else:
+            print(f"{self.name} is still a bit hungry.")
+
+
+my_pet = Pet("Buddy")
+my_pet.eat()
+my_pet.eat()
+my_pet.eat()
+my_pet.check_status()
