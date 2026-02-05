b873a74d6fb0a957f24ba208038c2b168b98085f
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-02-05 11:43:07 +0530

    Solved Task-2's comment

--- AdvancePython/Day-2 Constructors and Methods/Task-2.py
+++ AdvancePython/Day-2 Constructors and Methods/Task-2.py
@@ -1,12 +1,20 @@
 # Task 2: parameterized constructure
+from enum import Enum
+
+class Type(Enum):
+    ACTION = "Action"
+    COMEDY = "Comedy"
+    ROMANCE = "Romance"
+    HORROR = "Rorror"
+
 class Movie:
-    def __init__(self, name: str, type: str, cinema: str) -> None:
+    def __init__(self, name: str, type: Enum, cinema: str) -> None:
         self.name: str = name
-        self.type: str = type
+        self.type: Enum = type
         self.cinema: str = cinema
 
 
-movie_data = Movie("Avatar: Fire and Ash", "Action", "Hollywood")
+movie_data = Movie(name="Avatar: Fire and Ash", type=Type.ACTION, cinema="Hollywood")
 print(f"Movie name: {movie_data.name}")
-print(f"Movie type: {movie_data.type}")
+print(f"Movie type: {movie_data.type.value}")
 print(f"Movie cinema: {movie_data.cinema}")
