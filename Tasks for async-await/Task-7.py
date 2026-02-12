697ef4906c432be11957fb2205686a09c49d9289
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-02-12 19:10:56 +0530

    updated Task-7 of async/await according to comment

--- Tasks for async-await/Task-7.py
+++ Tasks for async-await/Task-7.py
@@ -1,33 +1,45 @@
 # Task-7: Write a program that uses multiple event loop to run different async functions
 import asyncio
-import threading
+import multiprocessing
+import os
+from typing import List, Coroutine, Any
 
 
-async def task(name: str) -> None:
-    print(f"{name} started...")
-    await asyncio.sleep(2)
-    print(f"{name} completed!")
+async def async_task(name: str, duration: float) -> None:
+    print(f"[Process {os.getpid()}] Task {name} starting...")
+    await asyncio.sleep(duration)
+    print(f"[Process {os.getpid()}] Task {name} finished after {duration}s.")
 
 
-def run_in_loop(async_func, name: str) -> None:
+def run_in_loop(tasks: List[Coroutine[Any, Any, None]]) -> None:
     loop = asyncio.new_event_loop()  # create new event loop
     asyncio.set_event_loop(loop)  # current thread's loop
-    loop.run_until_complete(async_func(name))  # run async function
-    loop.close()
+
+    try:
+        # coroutine, inside the process that will run them
+        tasks = [async_task(name, dur) for name, dur in tasks]
+        loop.run_until_complete(asyncio.gather(*tasks))  # run async function
+    finally:
+        loop.close()
 
 
 def main() -> None:
+    # define tasks for both processes
+    p1_tasks = [("A", 2), ("B", 1)]
+    p2_tasks = [("C", 3), ("D", 1)]
+
     # different threads using seperate event loops
-    thread1 = threading.Thread(target=run_in_loop, args=(task, "Loop-1"))
-    thread2 = threading.Thread(target=run_in_loop, args=(task, "Loop-2"))
+    process1 = multiprocessing.Process(target=run_in_loop, args=(p1_tasks,))
+    process2 = multiprocessing.Process(target=run_in_loop, args=(p2_tasks,))
 
-    thread1.start()  # start thread1
-    thread2.start()  # start thread2
+    print("Starting processes...")
+    process1.start()  # start thread1
+    process2.start()  # start thread2
 
-    thread1.join()  # wait for first thread to finish
-    thread2.join()  # wait for second thread to finish
+    process1.join()  # wait for first thread to finish
+    process2.join()  # wait for second thread to finish
 
-    print("All event loops completed!")
+    print("All processes have completed!")
 
 
 if __name__ == "__main__":
