16beb38db18237907ab64292e02f04aa486b51cd
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-02-12 18:37:13 +0530

    updated Task-6 of async/await according to comment

--- Tasks for async-await/Task-6.py
+++ Tasks for async-await/Task-6.py
@@ -7,36 +7,34 @@ from typing import Callable, Coroutine, Any
 
 # running async functon in thread
 def async_in_thread(async_func: Callable[[], Coroutine[Any, Any, None]]) -> None:
-    asyncio.run(async_func())
+    asyncio.run(async_func())  # creates new event loop for thread
 
 
 # async function
 async def async_task() -> None:
     print("async_task started...")
     await asyncio.sleep(2)
-    print("async_task done!")
+    print("async_task (async thread) done!")
 
 
 # normal function
 def normal_task() -> None:
     print("normal_task started...")
     time.sleep(1)
-    print("normal_task done!")
+    print("normal_task (main) done!")
 
 
-async def main() -> None:
-    # thread
-    thread: threading.Thread = threading.Thread(
-        target=async_in_thread, args=(async_task,)
-    )
+def main() -> None:
+    # thread to run the async function
+    thread = threading.Thread(target=async_in_thread, args=(async_task,))
     thread.start()
 
-    await asyncio.sleep(0.1)
+    time.sleep(0.1)
     normal_task()
 
     thread.join()
-    print("All tasks done!")
+    print("All tasks completed!")
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
