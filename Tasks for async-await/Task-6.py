0cabeb0cf63e5fa6f902879cbecd3c4b08829467
Pareexit-Baldaniya-Innvonix <pareexit.baldaniya@innvonix.in>
2026-02-13 12:40:41 +0530

    updated Task-6 of async/await according to comment

--- Tasks for async-await/Task-6.py
+++ Tasks for async-await/Task-6.py
@@ -6,15 +6,24 @@ from typing import Callable, Coroutine, Any
 
 
 # running async functon in thread
-def async_in_thread(async_func: Callable[[], Coroutine[Any, Any, None]]) -> None:
-    asyncio.run(async_func())  # creates new event loop for thread
+def async_in_thread(
+    async_func: Callable[[], Coroutine[Any, Any, None]], *args: Any, **kwargs: Any
+) -> None:
+    asyncio.run(async_func(*args, **kwargs))  # creates new event loop for thread
 
 
-# async function
+# async function without params
 async def async_task() -> None:
     print("async_task started...")
     await asyncio.sleep(2)
     print("async_task (async thread) done!")
+
+
+# async function with params
+async def async_task_params(name: str, delay: float) -> None:
+    print(f"{name} started (delay={delay})...")
+    await asyncio.sleep(delay)
+    print(f"{name} finished after {delay} sec!")
 
 
 # normal function
@@ -26,13 +35,20 @@ def normal_task() -> None:
 
 def main() -> None:
     # thread to run the async function
-    thread = threading.Thread(target=async_in_thread, args=(async_task,))
-    thread.start()
+    thread1 = threading.Thread(target=async_in_thread, args=(async_task,))
+    thread2 = threading.Thread(
+        target=async_in_thread, args=(async_task_params, "async_param_task", 3)
+    )
+
+    thread1.start()
+    thread2.start()
 
     time.sleep(0.1)
     normal_task()
 
-    thread.join()
+    thread1.join()
+    thread2.join()
+
     print("All tasks completed!")
 
 
